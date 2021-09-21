8c18fefc0988b8582787a9e05dd56caafbae3a79
Murilo Siqueira <msv2079@gmail.com>
2021-09-21 19:31:56 -0300

    Implementação de Module Pattern no Javascript

--- EsconderOptions/geral.js
+++ EsconderOptions/geral.js
@@ -1,11 +1,19 @@
-function CarregarOpcoes(elem) {
-    var tipoPessoa = elem.value.toLowerCase();
-    var opcoes = document.getElementById("tipo-pessoa-opcoes").getElementsByTagName("option");
-
-    var isPF = tipoPessoa == "pf";
-
-    opcoes[1].className = isPF ? "show" : "hide";
-    opcoes[2].className = isPF ? "show" : "hide";
-    opcoes[3].className = isPF ? "hide" : "show";
-    opcoes[4].className = isPF ? "hide" : "show";
-}
+/*
+    Exemplo de criação do module pattern retirado do link abaixo
+    https://addyosmani.com/resources/essentialjsdesignpatterns/book/#modulepatternjavascript
+*/
+var TipoPessoa = (function() {
+    return {
+        CarregarOpcoes: function(elem) {
+            var tipoPessoa = elem.value.toLowerCase();
+            var opcoes = document.getElementById("tipo-pessoa-opcoes").getElementsByTagName("option");
+    
+            var isPF = tipoPessoa == "pf";
+    
+            opcoes[1].className = isPF ? "show" : "hide";
+            opcoes[2].className = isPF ? "show" : "hide";
+            opcoes[3].className = isPF ? "hide" : "show";
+            opcoes[4].className = isPF ? "hide" : "show";
+        }
+    }
+})();
