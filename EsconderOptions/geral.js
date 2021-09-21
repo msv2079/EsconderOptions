f66117bfd6b3af8c243d0be180633f84524778a8
Murilo Siqueira <msv2079@gmail.com>
2021-09-21 19:39:42 -0300

    Remoção de .css e inclusão de jquery

--- EsconderOptions/geral.js
+++ EsconderOptions/geral.js
@@ -2,18 +2,25 @@
     Exemplo de criação do module pattern retirado do link abaixo
     https://addyosmani.com/resources/essentialjsdesignpatterns/book/#modulepatternjavascript
 */
-var TipoPessoa = (function() {
+var TipoPessoa = (function () {
     return {
-        CarregarOpcoes: function(elem) {
+        CarregarOpcoes: function (elem) {
             var tipoPessoa = elem.value.toLowerCase();
-            var opcoes = document.getElementById("tipo-pessoa-opcoes").getElementsByTagName("option");
-    
-            var isPF = tipoPessoa == "pf";
-    
-            opcoes[1].className = isPF ? "show" : "hide";
-            opcoes[2].className = isPF ? "show" : "hide";
-            opcoes[3].className = isPF ? "hide" : "show";
-            opcoes[4].className = isPF ? "hide" : "show";
+
+            var opcoes = $("#tipo-pessoa-opcoes option");;
+
+            if (tipoPessoa == "pf") {
+                $(opcoes[1]).show();
+                $(opcoes[2]).show();
+                $(opcoes[3]).hide();
+                $(opcoes[4]).hide();
+            }
+            else {
+                $(opcoes[1]).hide();
+                $(opcoes[2]).hide();
+                $(opcoes[3]).show();
+                $(opcoes[4]).show();
+            }
         }
     }
 })();
